ec0f86f74cb1e3208b5ed55642d6cd165144222c
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-06-17 23:05:38 +0200

    a

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,37 +1,33 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import styles from './Filter.module.css';
 import propTypes from 'prop-types';
 
-export class Filter extends Component {
-  state = {
-    filter: '',
-    names: this.props,
-  };
-  handleFilter = e => {
-    let value = e.target.value;
-    this.setState({ filter: value });
-    const namesArr = this.props.names.names.map(el => el.name);
-    let filteredNames = [];
-    namesArr.map(el => {
-      if (el.toLowerCase().includes(value.toLowerCase())) {
-        filteredNames.push(el);
-      }
-      return null;
-    });
-    console.log(filteredNames);
-    this.setState({ filter: filteredNames });
-    this.props.onChange(value);
-  };
-
-  render() {
-    return (
-      <div className={styles.filterBox}>
-        <h2>Find contacts by name</h2>
-        <input type="text" onChange={this.handleFilter} />
-      </div>
-    );
-  }
-}
+export const Filter = ({ handleFilterChange }) => {
+  // const state = {
+  //   filter: '',
+  // };
+  // handleFilter = e => {
+  //   let value = e.target.value;
+  //   this.setState({ filter: value });
+  //   const namesArr = this.props.names.names.map(el => el.name);
+  //   let filteredNames = [];
+  //   namesArr.map(el => {
+  //     if (el.toLowerCase().includes(value.toLowerCase())) {
+  //       filteredNames.push(el);
+  //     }
+  //     return null;
+  //   });
+  //   console.log(filteredNames);
+  //   this.setState({ filter: filteredNames });
+  //   this.props.onChange(value);
+  // };
+  return (
+    <div className={styles.filterBox}>
+      <h2>Find contacts by name</h2>
+      <input type="text" onChange={e => handleFilterChange(e)} />
+    </div>
+  );
+};
 
 Filter.propTypes = {
   handleFilter: propTypes.func,
