176b7be15b8e48b40b734c65be5357bd8bc5561a
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-06-28 16:51:35 +0200

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,26 @@ import { nanoid } from 'nanoid';
 export const App = () => {
   const [names, changeNames] = useState([]);
   const [filter, changeFilter] = useState([]);
-
-  useEffect(() => {
-    console.log('actualized');
-    const state = names.map(el => JSON.stringify(el));
-    console.log('state', state);
-    localStorage.setItem('state', JSON.stringify(state));
-  }, [names]);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     console.log('mounted');
-    if (localStorage.getItem('state')) {
-      const storedState = localStorage.getItem('state');
-      const parsedState = JSON.parse(storedState);
-      changeNames(parsedState);
-    }
+    setIsMounted(true);
+    const storedState = localStorage.getItem('state');
+    if (!storedState) return;
+    const parsedState = JSON.parse(storedState);
+    console.log(parsedState);
+    changeNames(parsedState);
   }, []);
+
+  useEffect(() => {
+    if (!isMounted) return;
+    console.log('actualized');
+    const state = JSON.stringify(names);
+    // if (state !== localStorage.getItem('state')) {
+    localStorage.setItem('state', state);
+    // }
+  }, [names]);
 
   const addNewName = (name, number) => {
     let isDuplicated = false;
